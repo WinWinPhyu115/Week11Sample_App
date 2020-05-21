bf51171eda8dc20d8ef848ae7d63dad2582c8e11
WinWinPhyu115 <s3733075@student.rmit.edu.au>
2020-05-21 15:52:03 +0000

    Tute 10 Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,10 +93,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.raise_delivery_errors=true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host='<tranquil-bayou-11673>.herokuapp.com'
-  config.action_mailer.default_url_options = {host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address=>'smtp.sendgrid.net',
     :port=>'587',
